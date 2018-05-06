7e1f0660d1e9cc857a7af6e786edc3cc5f1ea09c
Tekin Tian <tekintian@gmail.com>
2018-05-06 10:40:57 +0800

    hash算法完善
    Signed-off-by: Tekin Tian <tekintian@gmail.com>

--- src/main/java/cn/tekin/utils/Hash.java
+++ src/main/java/cn/tekin/utils/Hash.java
@@ -1,19 +1,35 @@
 package cn.tekin.utils;
 
 import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 /**
  * hash 加密算法，支持所有JAVA默认支持的加密方式， 如： MD2, MD5, SHA1, SHA-128,SHA-256, SHA-512, etc.
+ * 使用方法：
+ * 传递2个参数，默认加密方式为 SHA-512,或者传递的加密方式
+ * Hash.enCrypt("123",null)
+ * 只传递1个参数，默认加密方式为 MD5
+ * Hash.enCrypt("123")
+ * @Author TekinTian <tekintian@gmail.com>
  * @url http://dev.yunnan.ws
  */
 public class Hash {
 
-    public final static String enCrypt(String s) {
+    /**
+     * hash 加密算法实现
+     * 默认加密方式为 SHA-512,或者传递的加密方式
+     *
+     * @param msg      要加密的字符串
+     * @param hashType 加密类型 支持所有JAVA默认支持的加密方式， 如： MD2, MD5, SHA1, SHA-128,SHA-256, SHA-512, etc.
+     * @return 加密后的字符串
+     */
+    public final static String enCrypt(String msg, String hashType) {
         char hexDigits[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+        //默认加密方式为SHA-512
+        if (hashType == null || hashType.equals("")) hashType = "SHA-512";
+
         try {
-            byte[] strTemp = s.getBytes();
-            MessageDigest mdTemp = MessageDigest.getInstance("SHA-512");
+            byte[] strTemp = msg.getBytes();
+            MessageDigest mdTemp = MessageDigest.getInstance(hashType);
             mdTemp.update(strTemp);
             byte[] md = mdTemp.digest();
             int j = md.length;
@@ -32,17 +48,30 @@ public class Hash {
         }
     }
 
+    /**
+     * 不传加密类型，默认 MD5,
+     *
+     * @param msg
+     * @return
+     */
+    private static String enCrypt(String msg) {
+        return enCrypt(msg, "MD5");
+    }
+
 
     /**
      * main test
      * @param args
-     * @throws NoSuchAlgorithmException
      */
-    public static void main(String args[]) throws NoSuchAlgorithmException{
-        String msg = "郭XX-精品相声技术";
-        String enStr =enCrypt(msg);
-        System.out.println("密文是：" + enStr);
+    public static void main(String args[]) {
+
+        String msg = "888";
+
+        String enStr = Hash.enCrypt(msg);//加密
+
         System.out.println("明文是：" + msg);
+        System.out.println("密文是：" + enStr);
     }
+
 
 }  
